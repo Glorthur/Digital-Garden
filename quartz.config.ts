28faae6d07eee4f0fca90ea460fc8ba01c9292d1
Gloria Arthur <maamesi.b.arthur@gmail.com>
2026-04-04 11:00:24 +0000

    Set Quartz base URL for Digital-Garden

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "Glorthur.github.io",
+    baseUrl: "Glorthur.github.io/Digital-Garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
